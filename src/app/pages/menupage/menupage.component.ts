0bc2c62ceaa12ca29634e7251683dc54a82839ef
josepereza <josepereza66@gmail.com>
2023-08-27 09:15:41 +0200

    first commit3

--- src/app/pages/menupage/menupage.component.ts
+++ src/app/pages/menupage/menupage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-menupage',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./menupage.component.css']
 })
 export class MenupageComponent {
+menu!:any
+  constructor(private route:ActivatedRoute, private service:MenuService){
 
+  }
+  ngOnInit() {
+    const menuId =Number(this.route.snapshot.paramMap.get('id'));
+    this.menu=this.service.foodDetails.filter(data=>data.id==menuId)
+    console.log(this.menu[0])
+    
+  }
 }
